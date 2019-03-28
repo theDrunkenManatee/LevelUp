1d445f243670c4f533bbdbb93d99731100bbe51e
theDrunkenManatee <theDrunkenManatee@users.noreply.github.com>
2019-03-28 12:16:55 -0500

    made the DataParser class and relevant test class

---
+++ app/src/main/java/com/example/levelup/dataParser/DataParser.java
@@ -0,0 +1,22 @@
+package com.example.levelup.dataParser;
+
+public class DataParser {
+    private int shownX;
+    private int shownY;
+
+    public DataParser() {
+
+    }
+
+    public void parseAccelData(float x, float y, float z) {
+
+    }
+
+    //Getter Methods
+    public int getShownX() {
+        return shownX;
+    }
+    public int getShownY() {
+        return shownY;
+    }
+}
---
+++ app/src/test/java/com/example/levelup/dataParser/DataParserTest.java
@@ -0,0 +1,30 @@
+package com.example.levelup.dataParser;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataParserTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void parseAccelData() {
+    }
+
+    @Test
+    public void getShownX() {
+    }
+
+    @Test
+    public void getShownY() {
+    }
+}
