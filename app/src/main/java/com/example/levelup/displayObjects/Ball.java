32eb07d3a94e313e520cba6aff57519e062b7fe5
Kate Sanders <SandersKM@Hendrix.edu>
2019-03-29 16:28:16 -0500

    DisplayObject implemented in Ball

--- app/src/main/java/com/example/levelup/displayObjects/Ball.java
+++ app/src/main/java/com/example/levelup/displayObjects/Ball.java
@@ -1,47 +1,40 @@
 package com.example.levelup.displayObjects;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.RectF;
 
 import java.util.Random;
 
 // Adapted from http://androidgameprogramming.com/programming-a-pong-game/
 
-public class Ball {
+public class Ball implements DisplayObject{
     private RectF mRect;
     private float mXVelocity;
     private float mYVelocity;
-    private float mBallWidth;
-    private float mBallHeight;
+    private float mDiameter;
+    private float mRadius;
+    private Paint shapePaint;
 
     public Ball(int screenX, int screenY){
-
-        // Make the mBall size relative to the screen resolution
-        mBallWidth = screenX / 100;
-        mBallHeight = mBallWidth;
-
+        mDiameter = screenX / 100;
+        mRadius = mDiameter/2;
     /*
         Start the ball travelling straight up
         at a quarter of the screen height per second
     */
         mYVelocity = screenY / 4;
         mXVelocity = mYVelocity;
-
-        // Initialize the Rect that represents the mBall
         mRect = new RectF();
 
-    }
-
-    // Give access to the graphical representation of the Ball
-    public RectF getRect(){
-        return mRect;
     }
 
     // Change the position each frame
     public void update(long fps){
         mRect.left = mRect.left + (mXVelocity / fps);
         mRect.top = mRect.top + (mYVelocity / fps);
-        mRect.right = mRect.left + mBallWidth;
-        mRect.bottom = mRect.top - mBallHeight;
+        mRect.right = mRect.left + mDiameter;
+        mRect.bottom = mRect.top - mDiameter;
     }
 
     // Reverse the vertical heading
@@ -67,19 +60,40 @@ public class Ball {
 
     public void clearObstacleY(float y){
         mRect.bottom = y;
-        mRect.top = y - mBallHeight;
+        mRect.top = y - mDiameter;
     }
 
     public void clearObstacleX(float x){
         mRect.left = x;
-        mRect.right = x + mBallWidth;
+        mRect.right = x + mDiameter;
     }
 
     public void reset(int x, int y){
         mRect.left = x / 2;
         mRect.top = y - 20;
-        mRect.right = x / 2 + mBallWidth;
-        mRect.bottom = y - 20 - mBallHeight;
+        mRect.right = x / 2 + mDiameter;
+        mRect.bottom = y - 20 - mDiameter;
     }
 
+    public void setBallPosition(float xCenter, float yCenter){
+        mRect.left = xCenter - mRadius;
+        mRect.right = xCenter + mRadius;
+        mRect.top = yCenter + mRadius;
+        mRect.bottom = yCenter - mRadius;
+    }
+
+    @Override
+    public RectF getShape() {
+        return mRect;
+    }
+
+    @Override
+    public void setShapePaint(Paint paint) {
+        shapePaint = paint;
+    }
+
+    @Override
+    public void drawSelf(Canvas canvas) {
+        canvas.drawOval(getShape(), shapePaint);
+    }
 }
